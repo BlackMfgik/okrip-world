33bd3aae087f443da2056a934f53a228bb47d5c1
BlackMfgik <lanovui0902@gmail.com>
2026-04-03 00:02:23 +0300

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        servers: resolve(__dirname, "servers.html"),
+        map: resolve(__dirname, "map.html"),
+        "map-vanilla": resolve(__dirname, "map-vanilla.html"),
+        "map-creative": resolve(__dirname, "map-creative.html"),
+        "map-modded": resolve(__dirname, "map-modded.html"),
+      },
+    },
+  },
   server: {
     proxy: {
       "/bluemap": {
